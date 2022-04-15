e3aa531c8f0dec604d7772c01f9bc04ee063f697
Anderson Fortes <fortes.cc@gmail.com>
2022-04-15 10:58:34 -0300

    exemplos javascript

---
+++ type_numbers.js
@@ -0,0 +1,12 @@
+//tipo number
+
+const meu_numero = 3;
+
+console.log(meu_numero*8);
+
+const num2 = 3.4;
+//num2 = 4.3;
+
+
+
+console.log(meu_numero/num2);  
---
+++ type_string.js
@@ -0,0 +1,19 @@
+const ola = "Olá mundo";
+const nome = "Anderson";
+
+console.log(ola+nome)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "Belo Horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
